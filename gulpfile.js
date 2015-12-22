6f501db65d240a8bec34b97aab9c85376b6565f2
Kushal <kushal@elysian.net>
2015-12-22 23:22:03 +0000

    working with main-bower-files - bit buggy

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,51 @@
 var gulp = require('gulp');
 var $    = require('gulp-load-plugins')();
 var nunjucksRender = require('gulp-nunjucks-render');
- 
+
+// define plug-ins
+var flatten = require('gulp-flatten');
+var gulpFilter = require('gulp-filter');
+var uglify = require('gulp-uglify');
+var minifycss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var mainBowerFiles = require('main-bower-files');
+
+// Define paths variables
+var dest_path =  'public';
+// grab libraries files from bower_components, minify and push in /public
+gulp.task('publish-components', function() {
+  var jsFilter = gulpFilter('*.js', {restore: true}),
+      cssFilter = gulpFilter('*.css', {restore: true}),
+      fontFilter = gulpFilter(['*.eot', '*.woff', '*.svg', '*.ttf'], {restore: true});
+
+  return gulp.src(mainBowerFiles())
+
+  // grab vendor js files from bower_components, minify and push in /public
+  .pipe(jsFilter)
+  .pipe(gulp.dest(dest_path + '/js/'))
+  .pipe(uglify())
+  .pipe(rename({
+    suffix: ".min"
+  }))
+  .pipe(gulp.dest(dest_path + '/js/'))
+  .pipe(jsFilter.restore)
+
+  // grab vendor css files from bower_components, minify and push in /public
+  .pipe(cssFilter)
+  .pipe(gulp.dest(dest_path + '/css'))
+  .pipe(minifycss())
+  .pipe(rename({
+      suffix: ".min"
+  }))
+  .pipe(gulp.dest(dest_path + '/css'))
+  .pipe(cssFilter.restore)
+
+  // grab vendor font files from bower_components and push in /public
+  .pipe(fontFilter)
+  .pipe(flatten())
+  .pipe(gulp.dest(dest_path + '/fonts'));
+});
+
 gulp.task('nunjucks', function() {
   nunjucksRender.nunjucks.configure(['src/templates/']);
 
@@ -30,7 +74,7 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('default', ['sass', 'nunjucks'], function() {
+gulp.task('default', ['sass', 'nunjucks', 'publish-components'], function() {
   gulp.watch(['src/scss/**/*.scss'], ['sass']);
   gulp.watch(['src/templates/**/*.html'], ['nunjucks']);
 });
