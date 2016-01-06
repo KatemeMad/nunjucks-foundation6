54e8ba8b78f2e88d2f296aa9e8b5d0c3ec399a9e
Kushal Joshi <kushal@joshi.com>
2016-01-06 23:23:28 +0000

    rebuilt gulpfile, bower packages, template bower references

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,19 @@
-var gulp = require('gulp');
-var $    = require('gulp-load-plugins')();
-var nunjucksRender = require('gulp-nunjucks-render');
+const path = require('path');
+const gulp = require('gulp');
+const $    = require('gulp-load-plugins')();
+const nunjucksRender = require('gulp-nunjucks-render');
+const bower = require('gulp-bower');
 
-// define plug-ins
-var flatten = require('gulp-flatten');
-var gulpFilter = require('gulp-filter');
-var uglify = require('gulp-uglify');
-var minifycss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var mainBowerFiles = require('main-bower-files');
+const publicRoot = 'public';
+const bowerDir = 'src/bower_components';
 
-// Define paths variables
-var dest_path =  'public';
-// grab libraries files from bower_components, minify and push in /public
-gulp.task('publish-components', function() {
-  var jsFilter = gulpFilter('*.js', {restore: true}),
-      cssFilter = gulpFilter('*.css', {restore: true}),
-      fontFilter = gulpFilter(['*.eot', '*.woff', '*.svg', '*.ttf'], {restore: true});
-
-  return gulp.src(mainBowerFiles())
-
-  // grab vendor js files from bower_components, minify and push in /public
-  .pipe(jsFilter)
-  .pipe(gulp.dest(dest_path + '/js/'))
-  .pipe(uglify())
-  .pipe(rename({
-    suffix: ".min"
-  }))
-  .pipe(gulp.dest(dest_path + '/js/'))
-  .pipe(jsFilter.restore)
-
-  // grab vendor css files from bower_components, minify and push in /public
-  .pipe(cssFilter)
-  .pipe(gulp.dest(dest_path + '/css'))
-  .pipe(minifycss())
-  .pipe(rename({
-      suffix: ".min"
-  }))
-  .pipe(gulp.dest(dest_path + '/css'))
-  .pipe(cssFilter.restore)
-
-  // grab vendor font files from bower_components and push in /public
-  .pipe(fontFilter)
-  .pipe(flatten())
-  .pipe(gulp.dest(dest_path + '/fonts'));
+// publish bower libraries
+gulp.task('bower', function() {
+    return bower()
+    .pipe(gulp.dest( path.join(publicRoot, 'lib/') ))
 });
 
+// Nunjucks
 gulp.task('nunjucks', function() {
   nunjucksRender.nunjucks.configure(['src/templates/']);
 
@@ -54,12 +22,14 @@ gulp.task('nunjucks', function() {
   // Renders template with nunjucks
   .pipe(nunjucksRender())
   // output files in app folder
-  .pipe(gulp.dest('public'))
+  .pipe(gulp.dest(publicRoot))
 });
 
+
+// SASS
 var sassPaths = [
-  'src/bower_components/foundation-sites/scss',
-  'src/bower_components/motion-ui/src'
+  path.join(bowerDir, 'foundation-sites/scss'),
+  path.join(bowerDir, 'motion-ui/src')
 ];
 
 gulp.task('sass', function() {
@@ -71,10 +41,10 @@ gulp.task('sass', function() {
     .pipe($.autoprefixer({
       browsers: ['last 2 versions', 'ie >= 9']
     }))
-    .pipe(gulp.dest('public/css'));
+    .pipe(gulp.dest( path.join(publicRoot, 'css') ));
 });
 
-gulp.task('default', ['sass', 'nunjucks', 'publish-components'], function() {
+gulp.task('default', ['sass', 'nunjucks', 'bower'], function() {
   gulp.watch(['src/scss/**/*.scss'], ['sass']);
   gulp.watch(['src/templates/**/*.html'], ['nunjucks']);
 });
